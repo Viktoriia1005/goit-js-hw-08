0881f921646860cc9ee972b568a42fd74c88f241
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-15 23:38:29 +0200

    started task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+player.on('timeupdate', function (data) {
+  // data is an object containing properties specific to that event
+});
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
