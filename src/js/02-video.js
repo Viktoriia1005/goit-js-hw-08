3e8cc003b986a47b2b500a6dc87c6c1f447ce770
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-19 17:26:08 +0200

    correct task - 02, 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,9 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
+
 const player = new Player(iframe);
 
 // сохранение времени воспроизведения
@@ -12,10 +14,7 @@ if (savedTime) {
   player.setCurrentTime(savedTime);
 }
 // обновление времени воспроизведения.
-
-player.on(
-  'timeupdate',
-  throttle(data => {
-    localStorage.setItem(STORAGE_KEY, data.seconds.toString());
-  }, 1000),
-);
+function playTime(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds.toString());
+}
+player.on('timeupdate', throttle(playTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,10 +2,7 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
-
-form.addEventListener('input', throttle(onFormInput, 500));
-form.addEventListener('submit', onFormSubmit);
+let formData = {};
 
 updateForm();
 
@@ -13,6 +10,7 @@ function onFormInput(event) {
   formData[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
+form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(event) {
   event.preventDefault();
@@ -25,7 +23,9 @@ function onFormSubmit(event) {
   form.reset();
   localStorage.removeItem(STORAGE_KEY);
   console.log(formData);
+  formData = {};
 }
+form.addEventListener('submit', onFormSubmit);
 
 function updateForm() {
   const savedData = localStorage.getItem(STORAGE_KEY);
